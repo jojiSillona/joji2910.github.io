4fa7b8e8d804f739945fbe5412db306fb7a1385e
Eugene Sillona <93899090+jojiSillona@users.noreply.github.com>
2022-11-29 12:36:03 +0800

    final-edit

--- js/index.js
+++ js/index.js
@@ -41,18 +41,16 @@ window.onload = function() {
 }
 
 function showMusicDetails(music){
-    var musicDetail = document.getElementById('music-details')
-    var preview = document.getElementById('music-detail-body').children
-    var nodes
+    const musicTitle = document.getElementById('title-of-song')
+    const artist = document.getElementById('artist')
+    const spotify = document.getElementById('spotify')
+    const body = document.getElementById('detail-description')
     switch(music.name){
         case "we could be":
-            nodes = musicDetail.children
-
-            console.log(nodes)
-            nodes[0].innerHTML = "We Could Be"
-            nodes[1].innerHTML = "by \"Just A Fad\""
-            preview[0].setAttribute("src", "https://open.spotify.com/embed/track/4ci4yeWlhPhOvYzXRM9tPP?utm_source=generator")
-            preview[1].innerHTML = 
+            musicTitle.innerHTML = "We Could Be"
+            artist.textContent = "by \"Just A Fad\""
+            spotify.setAttribute("src", "https://open.spotify.com/embed/track/4ci4yeWlhPhOvYzXRM9tPP?utm_source=generator")
+            body.innerHTML = 
             `"We Could Be" was the first song that I've ever worked with which got 
             released in streaming services like Spotify, iTunes, and Apple Music. 
             As I was discovering my path to music production, I asked Just A Fad if 
@@ -60,32 +58,29 @@ function showMusicDetails(music){
             mix and they reuploaded their song with my mixdown.`
             break
         case "panaginip":
-            nodes = musicDetail.children
-            nodes[0].innerHTML = "Panaginip"
-            nodes[1].innerHTML = "by \"Sinai\""
-            preview[0].setAttribute("src", "https://open.spotify.com/embed/track/0CrXA4fbdlnBQVHNw8kfY9?utm_source=generator")
-            preview[1].innerHTML = 
+            musicTitle.innerHTML = "Panaginip"
+            artist.textContent = "by \"Sinai\""
+            spotify.setAttribute("src", "https://open.spotify.com/embed/track/0CrXA4fbdlnBQVHNw8kfY9?utm_source=generator")
+            body.innerHTML = 
             `"Panaginip" was dedicated to those who were too scared to make a move, ending up 
             daydreaming of how their relationship might have come to be. Written back in December 2019, 
             the song took 8 months to compose due to scheduling conflicts, most of which were of COVID's 
             fault.`
             break
         case "chica":
-            nodes = musicDetail.children
-            nodes[0].innerHTML = "Chica"
-            nodes[1].innerHTML = "by \"Akio Elijah\""
-            preview[0].setAttribute("src", "https://open.spotify.com/embed/track/1BPv3arYQCUtzF0YyFSF25?utm_source=generator")
-            preview[1].innerHTML = 
+            musicTitle.innerHTML = "Chica"
+            artist.textContent = "by \"Akio Elijah\""
+            spotify.setAttribute("src", "https://open.spotify.com/embed/track/1BPv3arYQCUtzF0YyFSF25?utm_source=generator")
+            body.innerHTML = 
             `"Chica" is a project by Akio Elijah. I was asked by him if I could mix and master his own song.
             Unlike my other songs, this song is more on a 'swing' vibe which was new to me as I was used to
             working with Indie Rock.`
             break
         case "babae nanaman":
-            nodes = musicDetail.children
-            nodes[0].innerHTML = "Babae Nanaman"
-            nodes[1].innerHTML = "by \"Just A Fad\""
-            preview[0].setAttribute("src", "https://open.spotify.com/embed/track/0SxZxPTejXfd5vBL5P71Jr?utm_source=generator")
-            preview[1].innerHTML = 
+            musicTitle.innerHTML = "Babae Nanaman"
+            artist.textContent = "by \"Just A Fad\""
+            spotify.setAttribute("src", "https://open.spotify.com/embed/track/0SxZxPTejXfd5vBL5P71Jr?utm_source=generator")
+            body.innerHTML = 
             `"Babae Nanaman" is a 4-track EP by Just A Fad. It was also my first time working with a full-fledged
             EP. Aside from mixing and mastering the song, this song also needed sound design to make some of the 
             songs a bit more 'full'. Initially consisting of 5 tracks, the EP included of "Babae Nanaman" an 
@@ -95,21 +90,19 @@ function showMusicDetails(music){
             break
 
         case "gloom":
-            nodes = musicDetail.children
-            nodes[0].innerHTML = "Gloom"
-            nodes[1].innerHTML = "by \"Akio Elijah\""
-            preview[0].setAttribute("src", "https://open.spotify.com/embed/track/4HY7r8dLgtVkD3upRdRwOk?utm_source=generator")
-            preview[1].innerHTML = 
+            musicTitle.innerHTML = "Gloom"
+            artist.textContent = "by \"Akio Elijah\""
+            spotify.setAttribute("src", "https://open.spotify.com/embed/track/4HY7r8dLgtVkD3upRdRwOk?utm_source=generator")
+            body.innerHTML = 
             `"Gloom" was another request from Akio Elijah for me to mix and master. Similar to his last single,
             it was way off my genre as it is more of like a 'street' beat to me. Regardless, it was fun to mix
             his song`
             break
         case "mhp":
-            nodes = musicDetail.children
-            nodes[0].innerHTML = "MHP(My Heart Prays)"
-            nodes[1].innerHTML = "by \"Sinai\""
-            preview[0].setAttribute("src", "https://open.spotify.com/embed/track/5H8uiTRfJGP5QajLVy4FaM?utm_source=generator")
-            preview[1].innerHTML = 
+            musicTitle.innerHTML = "MHP(My Heart Prays)"
+            artist.textContent = "by \"Sinai\""
+            spotify.setAttribute("src", "https://open.spotify.com/embed/track/5H8uiTRfJGP5QajLVy4FaM?utm_source=generator")
+            body.innerHTML = 
             `"MHP", a.k.a. "My Heart Prays" talks about being selfless in a relationship, where one party 
             keeps committing to the relationship even if the other party isn't that interested anymore. The 
             general composition of the song is really tough as I was handling around 30 different instruments, 
@@ -119,10 +112,3 @@ function showMusicDetails(music){
     }
 }
 
-// $(document).ready(function () {
-//     $("input").click(function() {
-//         $("#music-details").show()
-//         $("#music-details").animate({width: '1000px', height: '456px'}, 500)
-//         document.getElementById("music-details").scrollIntoView()
-//     })
-// })
